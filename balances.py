6fc11fe7de6d2976ac29eb4848a1b831b6876161
artrepreneur <46770824+artrepreneur@users.noreply.github.com>
2020-05-02 14:37:44 -0400

    Threaded it.

--- balances.py
+++ balances.py
@@ -1,8 +1,8 @@
 import subprocess
-import os, sys
+import os, sys, rpcworker
 from PyQt5.QtCore import *
-#from resource import resource_path
 from config import MIN_CONF, MAX_CONF
+from rpcworker import progress_fn, thread_complete
 _translate = QCoreApplication.translate
 
 def resource_path(relative_path):
@@ -10,10 +10,12 @@ def resource_path(relative_path):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath('.'), relative_path)
     
-def get_balance(uname, pwd):
+def get_balance(uname, pwd, progress_callback):
     bal_cmd_result, err = (subprocess.Popen([resource_path('bin/btcctl'), '-u', uname, '-P', pwd, '--wallet', 'getbalance'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate())
     bal_cmd_result = bal_cmd_result.decode('utf-8')
+    print('bal_cmd_result', bal_cmd_result)
     err = err.decode('utf-8')
+    print(bal_cmd_result, err)
 
     if not err:
         bal_cmd_result = round(float(bal_cmd_result),8)
@@ -30,3 +32,30 @@ def get_balance_for_addr(uname, pwd, addr):
             tmp_bal = round(float(itm["amount"]),4)
             bal += tmp_bal
         return str(bal)
+
+def get_balance_thd(u, p, win, state, pool):
+    global window, uname, pwd, worker_state_active
+    window = win
+    uname  = u
+    pwd = p
+    worker_state_active = state
+    threadpool = pool
+
+    # Pass the function to execute
+    if not worker_state_active['GET_BALANCE']:
+        worker_state_active['GET_BALANCE'] = True
+        worker = rpcworker.Worker(get_balance, uname, pwd) # Any other args, kwargs are passed to the run function
+        worker.signals.result.connect(print_result)
+        worker.signals.finished.connect(thread_complete)
+        worker.signals.progress.connect(progress_fn)
+
+        # Execute
+        threadpool.start(worker)        
+
+def print_result(result):
+    window.balance_amount.clear()
+    if result:
+        window.balance_amount.setText(_translate("MainWindow", result))
+    else:
+        window.balance_amount.setText(_translate("MainWindow", "Failed to retrieve balance."))
+    worker_state_active['GET_BALANCE'] = False
