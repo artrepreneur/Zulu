aa9f38a284e7f06a056a46a8e0386818430e8bdc
artrepreneur <46770824+artrepreneur@users.noreply.github.com>
2020-07-02 18:39:18 -0400

    fixes for code signing.

--- scripts/osx/make_osx.sh
+++ scripts/osx/make_osx.sh
@@ -22,15 +22,17 @@ mkdir -p "$BUILDDIR/deps"
 VERSION=$(git describe --tags --dirty --always)
 
 # Code Signing: See https://developer.apple.com/library/archive/documentation/Security/Conceptual/CodeSigningGuide/Procedures/Procedures.html
+echo "Set certs"
 echo $TRAVIS_OS_NAME
-export CERTIFICATE_P12=Certificate.p12
-echo $CERTIFICATE_OSX_P12 | base64 — decode > $CERTIFICATE_P12
-export KEYCHAIN=build.keychain #ios-build.keychain
+#export CERTIFICATE_P12=Certificate.p12
+#echo $CERTIFICATE_OSX_P12 | base64 — decode > $CERTIFICATE_P12
+#export KEYCHAIN=build.keychain #ios-build.keychain
 #security create-keychain -p mysecretpassword $KEYCHAIN
 #security default-keychain -s $KEYCHAIN
 #security unlock-keychain -p mysecretpassword $KEYCHAIN
 #security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign
-APP_SIGN='' #security find-identity -v
+#security find-identity -v
+APP_SIGN='' 
 
 if [ -n "$1" ]; then
     # Test the identity is valid for signing by doing this hack. There is no other way to do this.
