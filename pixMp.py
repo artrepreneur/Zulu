a0a70d3d8c7463afda443f9965abe48f5b2f1a21
artrepreneur <46770824+artrepreneur@users.noreply.github.com>
2020-10-08 13:48:18 -0400

    Additional graphics

--- pixMp.py
+++ pixMp.py
@@ -14,7 +14,7 @@ def resource_path(relative_path):
     
 def set_pixmaps():
     # Pixmap Imgs
-    global pixmap_balance_btn, pixmap_balance_btn2, pixmap_addr_btn, pixmap_addr_btn2
+    global pixmap_balance_btn, pixmap_balance_btn2, pixmap_addr_btn, pixmap_addr_btn2, pixmap_magic_btn, pixmap_magic_btn2
     global icons, pixmap_send_btn, pixmap_send_btn2, pixmap_receive_btn, pixmap_receive_btn2, pixmap_transaction_btn, pixmap_transaction_btn2
     pixmap_addr_btn = QPixmap(resource_path('img/glyphicons_325_wallet_blk@2x.png'))
     pixmap_addr_btn2 = QPixmap(resource_path('img/glyphicons_325_wallet_blk_alt@2x.png'))
@@ -26,5 +26,7 @@ def set_pixmaps():
     pixmap_receive_btn2 = QPixmap(resource_path('img/glyphicons_221_unshare_alt@2x.png'))
     pixmap_transaction_btn = QPixmap(resource_path('img/glyphicons_457_transfer@2x.png'))
     pixmap_transaction_btn2 = QPixmap(resource_path('img/glyphicons_457_transfer_alt@2x.png'))
-    icons = {'pixmap_addr_btn':pixmap_addr_btn, 'pixmap_addr_btn2':pixmap_addr_btn2, 'pixmap_balance_btn': pixmap_balance_btn, 'pixmap_balance_btn2':pixmap_balance_btn2, 'pixmap_send_btn':pixmap_send_btn, 'pixmap_send_btn2':pixmap_send_btn2, 'pixmap_receive_btn':pixmap_receive_btn, 'pixmap_receive_btn2':pixmap_receive_btn2,'pixmap_transaction_btn':pixmap_transaction_btn,'pixmap_transaction_btn2':pixmap_transaction_btn2}
+    pixmap_magic_btn = QPixmap(resource_path('img/glyphicons_081_refresh@2x.png'))
+    pixmap_magic_btn2 = QPixmap(resource_path('img/glyphicons_081_refresh_alt@2x.png'))
+    icons = {'pixmap_magic_btn':pixmap_magic_btn, 'pixmap_magic_btn2':pixmap_magic_btn2, 'pixmap_addr_btn':pixmap_addr_btn, 'pixmap_addr_btn2':pixmap_addr_btn2, 'pixmap_balance_btn': pixmap_balance_btn, 'pixmap_balance_btn2':pixmap_balance_btn2, 'pixmap_send_btn':pixmap_send_btn, 'pixmap_send_btn2':pixmap_send_btn2, 'pixmap_receive_btn':pixmap_receive_btn, 'pixmap_receive_btn2':pixmap_receive_btn2,'pixmap_transaction_btn':pixmap_transaction_btn,'pixmap_transaction_btn2':pixmap_transaction_btn2}
     return icons
